091ff7aa47d8ae0ebe71a980605eadeab655305f
Alwi Erdi Musthafa <114296469+alwierdi@users.noreply.github.com>
2024-06-13 09:22:54 +0700

    update transaction attributes (#1)
    * update transaction attributes
    
    * Delete Project_Lab_PSD directory

---
+++ Project_Lab_PSD-main/Project_Lab_PSD/Controller/TransactionController.cs
@@ -0,0 +1,26 @@
+﻿using Project_Lab_PSD.Modul;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace projectPSD.Project_Lab_PSD.Controller
+{
+    public class TransactionController
+    {
+        public static Response<TransaactionHeader> getAllHeaders()
+        {
+            return transaction.Handler.GetAllHeaders();
+        }
+
+        public static Response<List<TransaactionHeader>> getUserHeader(int userId)
+        {
+            if (userId == 0)
+            {
+                return new Response<List<TransaactionHeader>>(false, "User ID is required", null)
+            }
+
+            return transactionhandler
+        }
+    }
+}
---
+++ Project_Lab_PSD-main/Project_Lab_PSD/Factory/TransactionFactory.cs
@@ -0,0 +1,31 @@
+﻿using Project_Lab_PSD.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace projectPSD.Project_Lab_PSD.Factory
+{
+    public class TransactionFactory
+    {
+        public static TransaactionHeader createHeader(int userID, DateTime transactionDate, string status)
+        {
+            return new TransaactionHeader()
+            {
+                UserId = userID,
+                TransactionDate = transactionDate,
+                Status = status
+            };
+        }
+
+        public static TransactionDetail createDetail(int transactionID, int supplementID, int quantity)
+        {
+            return new TransactionDetail()
+            {
+                TransactionID = transactionID,
+                SupplementID = supplementID,
+                Quantity = quantity
+            };
+        }
+    }
+}
